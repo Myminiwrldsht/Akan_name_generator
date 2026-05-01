47b12655ed7a7b31f91ef3c55482c89251ede3cb
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-01 23:14:28 +0300

    feat:collect user's dob data

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const form = document.getElementById('akan-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();});
 
-   
+    const day = document.getElementById('day').value;
+    const month =document.getElementById('month').value;
+    const year = document.getElementById('year').value;
