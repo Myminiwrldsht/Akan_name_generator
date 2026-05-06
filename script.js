1ca7dacd8d3a5fea2046865e03958ebc11fae593
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-06 10:39:31 +0300

    fix: formatted the result statement

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ form.addEventListener('submit', function (event) {
 
 
     // Display the final result on the page
-    document.getElementById("result").innerText = "You were born on " + dayOfWeek + ". Your Akan Name is.... " + akanName + " 🎉";
+    document.getElementById("result").innerText = "You were born on a " + dayOfWeek + ". Your Akan Name is.... " + akanName + " 🎉";
 
 
     // Log values in the console for debugging
